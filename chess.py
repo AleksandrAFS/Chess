010a2220aa359c552990e03dde8c81e6ab83720d
Mirat <mirat1909@gmail.com>
2023-08-26 16:19:05 +0500

    Added abstract class Figure

--- chess.py
+++ chess.py
@@ -1,5 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class Figure(ABC):
+    def __init__(self, x, y, color):
+        self._x, self._y, self._color = x, y, color
+
+    @abstractmethod
+    def access_check(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def correct(self):
+        raise NotImplementedError
+    
+
 if __name__ == '__main__':
     pass
-
-
-print('hello world')
