88aea21b536717ac8c40e581728b32318decc6e1
Sasha <136837339+AleksandrAFS@users.noreply.github.com>
2023-08-27 12:24:51 +0300

    Update figures.py

--- figures.py
+++ figures.py
@@ -6,19 +6,16 @@ class Void:
     
     def __repr__(self):
         return ' '
-
+    
 
 class Figure(ABC):
     def __init__(self, color: bool, matr: list[list[int], list[int]]):
         self._color = color
         self._matr = matr
         
-
-    @abstractmethod
-    def access_check(self):
-        raise NotImplementedError
+    def access_check(self, x: int, y: int) -> bool|None:
+        return all(0 <= i < 8 for i in (x, y))
     
-    @abstractmethod
     def correct(self, row: int, col: int) -> bool:
         self._matr[self._x][self._y] = Void()
         self._matr[row][col] = self
@@ -30,25 +27,15 @@ class Figure(ABC):
         raise NotImplementedError
     
 
-class Elephant(Figure):
-    def access_check(self, x: int, y: int) -> bool:
-        return abs(self._x - x) == abs(self._y - y)
-
-    def correct(self):
-        pass
-
-    def __repr__(self) -> str:
-        return ('♝', '♗')[self._color]
-
 
 class Castle(Figure):
-    def access_check(self, x: int, y: int) -> bool:
-        return self._x == x or self._y == y
+    def access_check(self):
+        pass
     
     def correct(self):
         pass
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return ('♜', '♖')[self._color]
 
 
@@ -61,13 +48,16 @@ class Queen(Figure):
     def correct(self):
         pass
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return ('♛', '♕')[self._color]
 
 
 class Knight(Figure):
     def access_check(self, x: int, y: int) -> bool:
-        if (self._x - x) ** 2 + (self._y - y) ** 2 == 5:
+        if (
+            (self._x - x) ** 2 + (self._y - y) ** 2 == 5 
+            and super().access_check(x, y)
+        ):
             return self.correct(x, y)
             
     def correct(self, row: int, col: int) -> bool:
@@ -85,19 +75,18 @@ class King(Figure):
     def correct(self):
         pass
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return ('♚', '♔')[self._color]
     
 
 class Pawn(Figure):
     def access_check(self, x: int, y: int) -> bool:
         select = [1, -1][self._color]
-        prod = all(0 <= i < 8 for i in (x, y))
         
         if (
             self._x - x == select 
             and abs(self._y - y) <= 1 
-            and prod
+            and super().access_check(x, y)
            ):
             return self.correct(x, y, Figure if self._y != y else Void)
            
@@ -112,3 +101,18 @@ class Pawn(Figure):
             
     def __repr__(self) -> str:
         return ('♟', '♙')[self._color]
+            
+
+class Elephant(Figure):
+    def access_check(self, x: int, y: int) -> bool:
+        if (
+            abs(self._x - x) == abs(self._y - y)
+            and super().access_check(x, y)
+        ):
+            return self.correct(x, y)
+
+    def correct(self):
+        pass
+
+    def __repr__(self):
+        return ('♝', '♗')[self._color]
