a0819c8637228ce63b21ea1f42cc75de0b9d7aa1
Mirat <mirat1909@gmail.com>
2023-08-28 23:20:50 +0500

    Update figures.py

--- figures.py
+++ figures.py
@@ -19,13 +19,14 @@ class Figure(ABC):
         self._color = color
         self._matr = matr
 
-    def correct(self, row: int, col: int) -> None:
+    def correct(self, row: int, col: int) -> bool | None:
         whose_move = self._whose_move
         if self._color == whose_move:
             self._matr[self._x][self._y] = Void()
             self._matr[row][col] = self
             self._x, self._y = row, col
             type(self)._whose_move = (True, False)[whose_move]
+            return True
         
     @abstractmethod
     def access_check(self, x: int, y: int) -> bool:
@@ -37,15 +38,15 @@ class Figure(ABC):
     
 
 class Rook(Figure):
-    def access_check(self, x: int, y: int) -> None:
+    def access_check(self, x: int, y: int) -> bool | None:
         if (
             (self._x == x 
             or self._y == y) 
             and super().access_check(x, y)
         ):
-            self.correct(x, y)
+            return self.correct(x, y)
         
-    def correct(self, row: int, col: int) -> None:
+    def correct(self, row: int, col: int) -> bool | None:
         start, end = [row] * 8, [col] * 8
         
         match row:
@@ -61,52 +62,52 @@ class Rook(Figure):
             and all(isinstance(self._matr[i][j], Void) 
                     for i, j in zip(start, end))
         ):
-            super().correct(row, col)
+            return super().correct(row, col)
 
     def __repr__(self) -> str:
         return ('♜', '♖')[self._color]
 
 
 class Queen(Figure):
-    def access_check(self, x: int, y: int) -> None:
+    def access_check(self, x: int, y: int) -> bool | None:
         for figur in (Elephant, Rook):
             creats = figur(self._color, self._matr)
             creats._x, creats._y = self._x, self._y
             if creats.access_check(x, y):
-                super().correct(x, y)
+                return super().correct(x, y)
     
     def __repr__(self) -> str:
         return ('♛', '♕')[self._color]
 
 
 class Knight(Figure):
-    def access_check(self, x: int, y: int) -> None:
+    def access_check(self, x: int, y: int) -> bool | None:
         if (
             (self._x - x) ** 2 + (self._y - y) ** 2 == 5 
             and super().access_check(x, y)
         ):
-            self.correct(x, y)
+            return self.correct(x, y)
             
-    def correct(self, row: int, col: int) -> None:
+    def correct(self, row: int, col: int) -> bool | None:
         if self._matr[row][col]._color != self._color:
-            super().correct(row, col)
+            return super().correct(row, col)
 
     def __repr__(self) -> str:
         return ('♞', '♘')[self._color]
     
 
 class King(Figure):
-    def access_check(self, x: int, y: int) -> None:
+    def access_check(self, x: int, y: int) -> bool | None:
         if (
             abs(self._x - x) <= 1 
             and abs(self._y - y) <= 1
             and super().access_check(x, y)
         ):
-            self.correct(x, y)
+            return self.correct(x, y)
     
-    def correct(self, row: int, col: int) -> None:
+    def correct(self, row: int, col: int) -> bool | None:
         if self._matr[row][col]._color != self._color:
-            super().correct(row, col)
+            return super().correct(row, col)
 
     def __repr__(self) -> str:
         return ('♚', '♔')[self._color]
@@ -119,7 +120,7 @@ class Pawn(Figure):
         self.select = (1, -1)[self._color]
         self.run = (2, -2)[self._color]
         
-    def access_check(self, x: int, y: int) -> None:
+    def access_check(self, x: int, y: int) -> bool | None:
         if (
                (
                 (self._x - x == self.select 
@@ -131,9 +132,9 @@ class Pawn(Figure):
                 )
             and super().access_check(x, y)
            ):
-            self.correct(x, y, Figure if self._y != y else Void)
+            return self.correct(x, y, Figure if self._y != y else Void)
         
-    def correct(self, row: int, col: int, goto: object = Void) -> None:
+    def correct(self, row: int, col: int, goto: object = Void) -> bool | None:
         matr = self._matr[row]
         
         if (
@@ -141,21 +142,21 @@ class Pawn(Figure):
             and matr[col]._color != self._color
             ):
               self.start = False
-              super().correct(row, col)
+              return super().correct(row, col)
             
     def __repr__(self) -> str:
         return ('♟', '♙')[self._color]
             
 
 class Elephant(Figure):
-    def access_check(self, x: int, y: int) -> None:
+    def access_check(self, x: int, y: int) -> bool | None:
         if (
             abs(self._x - x) == abs(self._y - y)
             and super().access_check(x, y)
         ):  
-            self.correct(x, y)
+            return self.correct(x, y)
 
-    def correct(self, x: int, y: int) -> None:
+    def correct(self, x: int, y: int) -> bool | None:
         x_ = -1 if self._x > x else 1
         y_=  -1 if self._y > y else 1
         if (
@@ -165,7 +166,7 @@ class Elephant(Figure):
                     for i, j in zip(range(self._x + x_, x, x_), range(self._y + y_, y, y_))
                 )
         ):
-            super().correct(x, y)
+            return super().correct(x, y)
 
     def __repr__(self) -> str:
         return ('♝', '♗')[self._color] 
