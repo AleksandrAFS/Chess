0efb25fc3d310e172156d4736e205e3f4b5f486a
Sasha <136837339+AleksandrAFS@users.noreply.github.com>
2023-08-31 11:17:12 +0300

    Update board.py

--- board.py
+++ board.py
@@ -10,6 +10,7 @@ class Board:
         self.matrix = [[Void()] * 8 for _ in range(8)]
         self.types = (Rook, Knight, Elephant)
         self.last = ([], [])
+        self.kinges = []
         
         self.create(1, True)
         self.create(7, False)
@@ -20,6 +21,7 @@ class Board:
         return self.matrix[index]
             
     def create(self, side: int, color: bool) -> None:
+        
         #установка пешек
    
         for value in range(8):
@@ -30,11 +32,13 @@ class Board:
         for key in range(3):
             self.matrix[side - 1][key] = self.types[key](color, self.matrix)
             self.matrix[side - 1][~key] = self.types[key](color, self.matrix)
-            
-        #установка кароля и королевы
+        
+         #установка короля и королевы
         
         self.matrix[side - 1][3] = Queen(color, self.matrix)
-        self.matrix[side - 1][4] = King(color, self.matrix)
+        king = King(color, self.matrix)
+        self.matrix[side - 1][4] = king
+        self.kinges.append(king)
         
         if color is False:
             self.matrix[side - 1:] = [*reversed(self.matrix[side - 1:])]
@@ -46,6 +50,7 @@ class Board:
                 obj._x, obj._y = i, j
                 obj.last = self.last[not obj._color]
                 self.last[obj._color].append(obj)
-
+                obj.your_king = self.kinges[not obj._color]
+                
     def __repr__(self) -> str:
         return '\n'.join(' '.join(map(str, r)) for r in reversed(self.matrix))
